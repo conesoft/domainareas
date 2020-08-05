77ee98945d132d3d23ecdca26c842e33dc8dbfd4
David Spörri <david.spoerri@coop.ch>
2020-08-05 16:18:02 +0200

    first commit

---
+++ src/Conesoft.DomainAreas/SubdomainToAreaRewriter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+
+namespace Conesoft.DomainAreas
+{
+    public class SubdomainToAreaRewriter : IRule
+    {
+        public void ApplyRule(RewriteContext context)
+        {
+            var host = context.HttpContext.Request.Host.Host;
+            if (context.HttpContext.Request.Path.Value.StartsWith("/_blazor") == false)
+            {
+                context.HttpContext.Request.Path = "/" + host.Replace(".localhost", "") + context.HttpContext.Request.Path;
+            }
+        }
+    }
+}
---
+++ src/Conesoft.DomainAreas/SubdomainToAreaRewriterExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using System.Linq;
+
+namespace Conesoft.DomainAreas
+{
+
+    public static class SubdomainToAreaRewriterExtension
+    {
+        public static void UseSubdomainToAreaRewriter(this IApplicationBuilder app, bool withCascadingStaticFiles = false)
+        {
+            if(withCascadingStaticFiles)
+            {
+                app.UseStaticFilesForSubdomains();
+            }
+
+            var rewriter = new RewriteOptions();
+            rewriter.Add(new SubdomainToAreaRewriter());
+            app.UseRewriter(rewriter);
+        }
+
+        private static void UseStaticFilesForSubdomains(this IApplicationBuilder app)
+        {
+            var env = app.ApplicationServices.GetService(typeof(IWebHostEnvironment)) as IWebHostEnvironment;
+            foreach (var directory in (Files.Directory.From(env.ContentRootPath) / "Areas").Directories.Where(d => d.Directories.Any(sd => sd.Name == "wwwroot")))
+            {
+                app.UseMiddleware<AspNetCore.StaticFiles.StaticFileMiddleware>(Options.Create(new AspNetCore.Builder.StaticFileOptions
+                {
+                    ShouldSkip = ctx => ctx.Context.Request.Host.Host.Split(':').First() != directory.Name && ctx.Context.Request.Host.Host.Split(':').First() != directory.Name + ".localhost",
+                    FileProvider = new PhysicalFileProvider((directory / "wwwroot").Path)
+                }));
+            }
+
+            app.UseStaticFiles();
+        }
+    }
+}
